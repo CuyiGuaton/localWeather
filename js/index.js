604fb07175e42cecdfcf6a852623d93d3a4b5127
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-09 20:23:35 -0400

    agregada la api del clima y muestra la temperatura, no sabes lo dificil que fue esto

--- js/index.js
+++ js/index.js
@@ -3,9 +3,17 @@ function getCurrentLocation(){
   $.getJSON("http://ip-api.com/json/?callback=", function(data){
     var lat=data.lat;
     var lon= data.lon;
-    $("#pico").html(lon);
+    getCurrentWeather(lat, lon);
   });
-} 
+}
+
+function getCurrentWeather(lat, lon){
+  $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=424337f4099555b5b5d0da826201e9c2", function(data){
+
+    alert( data.main.temp);
+
+  });
+}
 
 $(document).ready(function() {
   getCurrentLocation();
