c406cb037db9cd015efb986e299fce2787894ee3
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-09 22:37:43 -0400

    agregado iconos, un cuadrado en mitad de la página y una imagen de rem <3

--- js/index.js
+++ js/index.js
@@ -10,11 +10,13 @@ function getCurrentLocation(){
 function getCurrentWeather(lat, lon){
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=424337f4099555b5b5d0da826201e9c2", function(data){
 
-    $("#temp").html(data.main.temp);
+    $("#temp").html(Math.round(data.main.temp - 273.15) + "°C");
 
   });
 }
 
 $(document).ready(function() {
   getCurrentLocation();
+  $('body').css('background-image', 'url(background/rem.png)');
+
 });
