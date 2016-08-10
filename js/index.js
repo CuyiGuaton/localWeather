997caf37ef16b50eefc29e608b6fc5a422dec3b2
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-10 01:57:32 -0400

    el clima cambia según el día,la noche y la condición atmosferica, se agrego un footer

--- js/index.js
+++ js/index.js
@@ -10,11 +10,15 @@ function getCurrentLocation(){
 function getCurrentWeather(lat, lon){
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=424337f4099555b5b5d0da826201e9c2", function(data){
     $("#temp").html(Math.round(data.main.temp - 273.15) + "°C");
-
+    if(data.weather[0].icon[2] === "d"){
+      $(".wi").addClass("wi-owm-day-"+ data.weather[0].id);
+    } else{
+      $(".wi").addClass("wi-owm-night-"+ data.weather[0].id);
+    }
   });
 }
 
 $(document).ready(function() {
   getCurrentLocation();
-  $('body').css('background-image', 'url(background/rem.png)');
+  $('body').css('background-image', 'url(background/earth.jpg)');
 });
