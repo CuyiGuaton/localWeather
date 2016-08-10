8e7ff2e615f3aee45b61d0ff7becf487ca7dc13b
Sergio Salinas <sergiosalfer@gmail.com>
2016-08-10 00:03:55 -0400

    Se agregaron banderas y  se integraron con js

--- js/index.js
+++ js/index.js
@@ -1,15 +1,14 @@
 
 function getCurrentLocation(){
   $.getJSON("http://ip-api.com/json/?callback=", function(data){
-    var lat=data.lat;
-    var lon= data.lon;
-    getCurrentWeather(lat, lon);
+    $("#city").html(data.city );
+    $(".flag-icon").addClass("flag-icon-" + data.countryCode.toLowerCase());
+    getCurrentWeather(data.lat, data.lon);
   });
 }
 
 function getCurrentWeather(lat, lon){
   $.getJSON("http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon +"&appid=424337f4099555b5b5d0da826201e9c2", function(data){
-
     $("#temp").html(Math.round(data.main.temp - 273.15) + "°C");
 
   });
@@ -17,6 +16,5 @@ function getCurrentWeather(lat, lon){
 
 $(document).ready(function() {
   getCurrentLocation();
-  $('body').css('background-image', 'url(background/rem.png)');
-
+  $('body').css('background-image', 'url(background/earth.jpg)');
 });
